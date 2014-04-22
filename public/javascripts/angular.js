231777530973e0886dfd4e2fac4fea4771f37a37
agharib <ahmedg1@umbc.edu>
2014-04-21 21:16:05 -0400

    New Feautures: all fields must be valid (thanks to dustin), email must be proper format, passwords must match, passwords should be 8 digits long min and contain at lease one number, passwords must match, phone must be a digit long number (bug) couldn't get phone number validation to work)

--- public/javascripts/angular.js
+++ public/javascripts/angular.js
@@ -1,3 +1,4 @@
+
 app = angular.module('LoginApp', []);
 app.directive('modalDialog', function() {
   return {
@@ -20,6 +21,57 @@ app.directive('modalDialog', function() {
     template: "<div class='ng-modal' ng-show='show'><div class='ng-modal-overlay' ng-click='hideModal()'></div><div class='ng-modal-dialog' ng-style='dialogStyle'><div class='ng-modal-close' ng-click='hideModal()'>X</div><div class='ng-modal-dialog-content' ng-transclude></div></div></div>"
   };
 });
+
+function stageController($scope) {
+
+    $scope.username1 = 'Peter Parker';
+    $scope.email1 = 'pparker@gmail.com';
+	$scope.inputUserPassword = 'password';
+	
+    $scope.submitForm = function () {
+        console.info("Here I should implement the logic to send a request to the server.");
+    }
+
+}
+
+app.directive('passwordValidate', function() {
+    return {
+        require: 'ngModel',
+        link: function(scope, elm, attrs, ctrl) {
+            ctrl.$parsers.unshift(function(viewValue) {
+
+                scope.pwdValidLength = (viewValue && viewValue.length >= 8 ? 'valid' : undefined);
+                scope.pwdHasLetter = (viewValue && /[A-z]/.test(viewValue)) ? 'valid' : undefined;
+                scope.pwdHasNumber = (viewValue && /\d/.test(viewValue)) ? 'valid' : undefined;
+
+                if(scope.pwdValidLength && scope.pwdHasLetter && scope.pwdHasNumber) {
+                    ctrl.$setValidity('pwd', true);
+                    return viewValue;
+                } else {
+                    ctrl.$setValidity('pwd', false);                    
+                    return undefined;
+                }
+
+            });
+        }
+    };
+});
+
+app.directive('pwCheck', [function () {
+                return {
+                    require: 'ngModel',
+                    link: function (scope, elem, attrs, ctrl) {
+
+						var me = attrs.ngModel;
+						var matchTo = attrs.pwCheck;
+
+						scope.$watch('[me, matchTo]', function(value){
+							ctrl.$setValidity('pwmatch', scope[me] === scope[matchTo] );
+						});
+
+                    }
+                }
+            }]);
 
 app.controller('MyCtrl', ['$scope', function($scope) {
   $scope.modalShown = false;
